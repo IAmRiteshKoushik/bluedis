973f7a35ee60699743050b5d6aa4a782f1a13e34
yendelevium <yashbardia27@gmail.com>
2024-12-29 21:08:45 +0000

    Optimize bloom-filter
    - Using bits instead of bool to store the probabilistic value in the bloom-filter
    - Refactor data to be of type string instead of []byte
    - Add a test to see how the bloom-filter false-positivity rate scales wrt bloom-filter size

--- bloomFilter.go
+++ bloomFilter.go
@@ -5,68 +5,87 @@ import (
 	"hash"
 	"hash/fnv"
 	"strconv"
+	"time"
 
 	"github.com/pierrec/xxHash/xxHash32"
 	"github.com/twmb/murmur3"
 )
 
 type BloomFilter struct {
-	filter []bool
+	filter []uint8
 	size   uint32
 }
 
-// var mHasher hash.Hash32 = murmur3.SeedNew32(uint32(time.Now().Unix()))
-// TODO : Change the seeds to time.Now().Unix() AFTER Testing
-
 // All the hashfunctions will be stored in this map
 var hashFunctions = map[string]hash.Hash32{
-	"murmurhash": murmur3.SeedNew32(uint32(12)),
+	"murmurhash": murmur3.SeedNew32(uint32(time.Now().Unix())),
 	"fnvhash":    fnv.New32(),
-	"xxhash":     xxHash32.New(19),
+	"xxhash":     xxHash32.New(uint32(time.Now().Unix())),
 }
 
 // A common hash function that takes in the hasher and returns the hash
-func getHash(hasher hash.Hash32, data []byte) uint32 {
-	hasher.Write(data)
+func getHash(hasher hash.Hash32, data string) uint32 {
+	hasher.Write([]byte(data))
 	hashedSum := hasher.Sum32()
 	hasher.Reset()
 	return hashedSum
 }
 
 // For each idx given by hash%size, mark that bloom filter bucket as true
-// TODO: Instead of using bool, use a uint8 and change just the bit to either 1 or 0
-func (bf *BloomFilter) Add(data []byte) {
+// We use bit manipulation to propogate the bitIdx to the appropriate bit and set it to 1
+
+// Idx Calculations
+// The total size of the bloom filter is the size*8, as we are storing the 1's in BITS
+// byteIdx is hashIdx/8, as technically we only have "size" no.of array indexes not "size*8"
+// bitIdx is hashIdx%8 to get the exact bit in the byte
+func (bf *BloomFilter) Add(data string) {
 	for _, hasher := range hashFunctions {
-		hashIdx := getHash(hasher, data) % bf.size
-		bf.filter[hashIdx] = true
+		hashIdx := getHash(hasher, data) % (bf.size * 8)
+		byteIdx := hashIdx / 8
+		bitIdx := hashIdx % 8
+		// fmt.Println(hashIdx, byteIdx, bitIdx)
+		bf.filter[byteIdx] |= (1 << bitIdx)
 	}
 }
 
 // IF there exists "true" for every hash%size in the filter, it means the element exists in the filter
 // Even if one hash is off, the element doesn't exist, return false
-func (bf *BloomFilter) Exists(data []byte) bool {
+func (bf *BloomFilter) Exists(data string) bool {
 	for _, hasher := range hashFunctions {
-		hashIdx := getHash(hasher, data) % bf.size
-		if !bf.filter[hashIdx] {
+		hashIdx := getHash(hasher, data) % (bf.size * 8)
+		byteIdx := hashIdx / 8
+		bitIdx := hashIdx % 8
+		if (bf.filter[byteIdx] & (1 << bitIdx)) == 0 {
 			return false
 		}
 	}
 	return true
 }
 
-// TODO: Instead of using bool, use a uint8 and change just the bit to either 1 or 0
 func NewBloomFilter(size uint32) BloomFilter {
 	return BloomFilter{
-		filter: make([]bool, size),
+		filter: make([]uint8, size),
 		size:   size,
 	}
 }
 
 func bloomFilter() {
-	bloom := NewBloomFilter(1000)
-	fmt.Println(bloom.Exists([]byte("770")))
-	for i := 0; i < 750; i++ {
-		bloom.Add([]byte(strconv.Itoa(i)))
+	// Testing to see how the false-positivity rate scales as compared to bloomfilter size
+	for j := 75000; j < 100000; j += 500 {
+		bloom := NewBloomFilter(uint32(j))
+		// Adding 75000 elements
+		// Checking false positivity for 25000 elements
+		for i := 0; i < 75000; i++ {
+			bloom.Add(strconv.Itoa(i))
+		}
+
+		falsePositives := 0.0
+		for i := 75000; i < 100000; i++ {
+			if bloom.Exists(strconv.Itoa(i)) {
+				falsePositives++
+			}
+		}
+		fmt.Println(falsePositives / 25000)
 	}
-	fmt.Println(bloom.Exists([]byte("770")))
+
 }
